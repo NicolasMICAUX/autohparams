fd11099ca8d4527035b2d474ff926b9cfef6ca3a
NicolasMICAUX <micaux.nicolas@gmail.com>
2023-07-26 21:42:20 +0200

    correct test

--- tests/test_auto_config.py
+++ tests/test_auto_config.py
@@ -1,9 +1,13 @@
 from unittest import TestCase
+
 from autoconfig.auto_config import get_auto_config
 
 
 class MyClass:
-    pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 
 x = 0
 _y = 1
@@ -11,4 +15,4 @@ _y = 1
 
 class Test(TestCase):
     def test_auto_config(self):
-        assert get_auto_config(globals()) == {'MyClass': 'MyClass', 'x': 0}
+        assert get_auto_config(globals()) == {"x": 0}
